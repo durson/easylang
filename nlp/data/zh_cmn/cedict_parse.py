41a33522a782cf43cf2c19560aba26246534c189
Piotr Dura <piotrdura7@gmail.com>
2020-12-18 18:37:55 +0100

    added cedict parser

--- nlp/data/zh_cmn/cedict_parse.py
+++ nlp/data/zh_cmn/cedict_parse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-import json
+import orjson as json
 
 from pathlib import Path
 
@@ -27,6 +27,34 @@ def parse_line(line):
     return parsed
 
 
+def get_vocab(cedict):
+    vocab_parsed = {}
+    vocab = {}
+    for parsed in cedict:
+        chars = list(parsed["simplified"])
+        if len(chars) == 1:
+            vocab_parsed[chars[0]] = parsed
+        for char in chars:
+            vocab[char] = 1
+    vocab = sorted(vocab.keys())
+
+    def retrieve_char_or_dummy(char):
+        if char in vocab_parsed:
+            return vocab_parsed[char]
+        else:
+            english = f"Letter {char}"
+            if char in list("0123456789"):
+                english = f"Number {char}"
+            return {
+                "traditional": char,
+                "simplified": char,
+                "pinyin": char,
+                "english": english
+            }
+
+    return list(map(retrieve_char_or_dummy, vocab))
+
+
 def filter_surnames(cedict):
     for i in range(len(cedict)-1, -1, -1):
         if "surname " in cedict[i]["english"]:
@@ -35,7 +63,10 @@ def filter_surnames(cedict):
 
 
 def main(argv):
-    cedict_path = Path("cedict_1_0_ts_utf-8_mdbg.txt")
+    if len(argv) != 3:
+        raise ValueError("Usage: cedict-path output-dir")
+
+    cedict_path = Path(argv[1])
     if not cedict_path.exists():
         raise RuntimeError(f"{cedict_path} does not exist.")
 
@@ -47,9 +78,19 @@ def main(argv):
             if parsed is not None:
                 cedict.append(parsed)
     filter_surnames(cedict)
+
+    output_dir = Path(argv[2])
+    if not output_dir.exists():
+        output_dir.mkdir(exist_ok=True)
+
+    vocab = get_vocab(cedict)
+    with open(output_dir.joinpath("vocab.json"), "w") as fp:
+        fp.write(json.dumps(vocab).decode())
+    with open(output_dir.joinpath("dict.json"), "w") as fp:
+        fp.write(json.dumps(cedict).decode())
     return cedict
 
 
 parsed_dict = main(sys.argv)
 
-print("\n".join(map(lambda d: str(d), parsed_dict)))
+print("\n".join(map(str, parsed_dict)))
